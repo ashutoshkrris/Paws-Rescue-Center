633d72c2fad32f70f0dcf014111554a51adbbae3
Ashutosh Krishna <ashutoshbritish@gmail.com>
2020-08-08 17:07:42 +0530

    Project Challenge 8

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 """Flask Application for Paws Rescue Center."""
 from flask import Flask, render_template, abort
-from forms import SignUpForm
+from forms import SignUpForm, LoginForm
+from flask import session, redirect, url_for
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'EIwLfnXkzRtxyWA5UCMuZ80gDcHp317oOasi9ShrYNdljqJKvGeFPBT24Vb6Qm'
@@ -16,6 +17,7 @@ pets = [
 """Information regarding the Users in the System."""
 users = [
             {"id": 1, "full_name": "Pet Rescue Team", "email": "team@pawsrescue.co", "password": "adminpass"},
+            {"id": 2, "full_name": "Ashutosh Krishna", "email": "ask@ak.com", "password": "123456789"}
         ]
 
 
@@ -46,6 +48,24 @@ def signup():
         users.append(new_user)
         return render_template("signup.html", message = "Successfully signed up")
     return render_template("signup.html", form = form)
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = next((user for user in users if user["email"] == form.email.data and user["password"] == form.password.data), None)
+        if user is None:
+            return render_template("login.html", form = form, message = "Wrong Credentials. Please Try Again.")
+        else:
+            session['user'] = user
+            return render_template("login.html", message = "Successfully Logged In!")
+    return render_template("login.html", form = form)
+
+@app.route("/logout")
+def logout():
+    if 'user' in session:
+        session.pop('user')
+    return redirect(url_for('home', _scheme='https', _external=True))
 
 if __name__ == "__main__":
     """ """
